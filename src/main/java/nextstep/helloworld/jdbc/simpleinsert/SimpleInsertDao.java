1806c07b8f320ccfa92e714f0f4e939f6640936b
RIANAEH <ririhan217@gmail.com>
2022-04-21 00:46:13 +0900

    학습 테스트 공부 완료^^

--- src/main/java/nextstep/helloworld/jdbc/simpleinsert/SimpleInsertDao.java
+++ src/main/java/nextstep/helloworld/jdbc/simpleinsert/SimpleInsertDao.java
@@ -1,6 +1,10 @@
 package nextstep.helloworld.jdbc.simpleinsert;
 
+import java.util.HashMap;
+import java.util.Map;
 import nextstep.helloworld.jdbc.Customer;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
@@ -17,20 +21,27 @@ public class SimpleInsertDao {
     }
 
     /**
-     * Map +
-     * insertActor.executeAndReturnKey
-     * id를 포함한 Customer 객체를 반환하세요
+     * Map + insertActor.executeAndReturnKey id를 포함한 Customer 객체를 반환하세요
      */
     public Customer insertWithMap(Customer customer) {
-        return null;
+        Map<String, Object> parameters = new HashMap<>(2);
+        parameters.put("first_name", customer.getFirstName());
+        parameters.put("last_name", customer.getLastName());
+
+        Long newId = insertActor.executeAndReturnKey(parameters).longValue();
+
+        return new Customer(newId, customer.getFirstName(), customer.getLastName());
     }
 
     /**
-     * SqlParameterSource +
-     * insertActor.executeAndReturnKey
-     * id를 포함한 Customer 객체를 반환하세요
+     * SqlParameterSource + insertActor.executeAndReturnKey id를 포함한 Customer 객체를 반환하세요
      */
     public Customer insertWithBeanPropertySqlParameterSource(Customer customer) {
-        return null;
+        SqlParameterSource paramSource = new MapSqlParameterSource(
+                Map.of("first_name", customer.getFirstName(), "last_name", customer.getLastName()));
+
+        Long newId = insertActor.executeAndReturnKey(paramSource).longValue();
+
+        return new Customer(newId, customer.getFirstName(), customer.getLastName());
     }
 }
