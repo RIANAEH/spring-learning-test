1fe3978f1816696cfdc7838807d7d4bc51f10f69
RIANAEH <ririhan217@gmail.com>
2022-04-21 01:45:56 +0900

    학습 테스트 공부 완료^^

--- src/main/java/nextstep/helloworld/core/di/StationConstructorService.java
+++ src/main/java/nextstep/helloworld/core/di/StationConstructorService.java
@@ -1,7 +1,16 @@
 package nextstep.helloworld.core.di;
 
+import java.beans.ConstructorProperties;
+import org.springframework.stereotype.Service;
+
+@Service
 public class StationConstructorService {
     private StationRepository stationRepository;
+
+    @ConstructorProperties({"stationRepository"})
+    public StationConstructorService(StationRepository stationRepository) {
+        this.stationRepository = stationRepository;
+    }
 
     public String sayHi() {
         return stationRepository.sayHi();
