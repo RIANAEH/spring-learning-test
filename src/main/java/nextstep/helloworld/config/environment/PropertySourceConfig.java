cfcb86214ef91baa3eb17fafd4fd3856c87f3d05
RIANAEH <ririhan217@gmail.com>
2022-05-26 22:41:04 +0900

    ♻️: @PropertySource의 value를 컨벤션에 맞게 변경

--- src/main/java/nextstep/helloworld/config/environment/PropertySourceConfig.java
+++ src/main/java/nextstep/helloworld/config/environment/PropertySourceConfig.java
@@ -8,7 +8,7 @@ import org.springframework.core.env.Environment;
 // TODO: Java-based Configuration을 하기 위한 클래스로 지정하기
 // TODO: application.properties 파일을 활용하기 위한 설정 추가하기
 @Configuration
-@PropertySource("application.properties")
+@PropertySource("classpath:application.properties")
 public class PropertySourceConfig {
 
     private final Environment env;
